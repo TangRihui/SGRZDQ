08bfaeedadb2538047528499c40624826ce6cc04
Tang Rihui <tangrihui@hotmail.com>
2022-01-15 23:39:03 +0800

    V1.15

--- miniprogram/pages/register/register.js
+++ miniprogram/pages/register/register.js
@@ -1,8 +1,11 @@
 // miniprogram/pages/home/home.js
-var pageStatus = "unknown"
+const app = getApp()
+var pageStatus = 'unknown'
 Page({
   data: {
-    pageStatus: pageStatus
+    pageStatus: pageStatus,
+    navTitle: '注册/登录',
+    back: false,
   },
 
   /**
@@ -10,20 +13,76 @@ Page({
    */
   onLoad: function (options) {
     var userInfo = wx.getStorageSync('userInfo')
-    if (userInfo =="") {
-      pageStatus = "unknown"
+    if (userInfo == '') {
+      pageStatus = 'unknown'
     }
     else {
 
     }
     console.log(pageStatus)
+    if (!wx.cloud) {
+      wx.redirectTo({
+        url: '../chooseLib/chooseLib',
+      })
+      return
+    }
+    if (wx.getUserProfile) {
+      this.setData({
+        canIUseGetUserProfile: true,
+      })
+    }
   },
 
+  getUserProfile() {
+    // 推荐使用wx.getUserProfile获取用户信息，开发者每次通过该接口获取用户个人信息均需用户确认，开发者妥善保管用户快速填写的头像昵称，避免重复弹窗
+    wx.getUserProfile({
+      desc: '展示用户信息', // 声明获取用户个人信息后的用途，后续会展示在弹窗中，请谨慎填写
+      success: (res) => {
+        this.setData({
+          avatarUrl: res.userInfo.avatarUrl,
+          userInfo: res.userInfo,
+          hasUserInfo: true,
+        })
+      }
+    })
+  },
+
+  onGetUserInfo: function (e) {
+    if (!this.data.logged && e.detail.userInfo) {
+      this.setData({
+        logged: true,
+        avatarUrl: e.detail.userInfo.avatarUrl,
+        userInfo: e.detail.userInfo,
+        hasUserInfo: true,
+      })
+    }
+  },
+
+  onGetOpenid: function () {
+    // 调用云函数
+    wx.cloud.callFunction({
+      name: 'login',
+      data: {},
+      success: res => {
+        console.log('[云函数] [login] user openid: ', res.result.openid)
+        app.globalData.openid = res.result.openid
+        wx.navigateTo({
+          url: '../userConsole/userConsole',
+        })
+      },
+      fail: err => {
+        console.error('[云函数] [login] 调用失败', err)
+        wx.navigateTo({
+          url: '../deployFunctions/deployFunctions',
+        })
+      }
+    })
+  },
   /**
    * 生命周期函数--监听页面初次渲染完成
    */
   onReady: function () {
-    
+
   },
 
   /**
